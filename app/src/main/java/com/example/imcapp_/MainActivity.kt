d1253c81e9fc6e46ad977ba8db5f33bd9fb50975
LUAN ENZO SIMONATO FERRASIN <21193394@CFP127ALUNO.LOCAL>
2021-10-04 16:55:44 -0300

    segunda tela realizada

--- app/src/main/java/com/example/imcapp_/MainActivity.kt
+++ app/src/main/java/com/example/imcapp_/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.imcapp_
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar!!.hide()
+
+        val buttonEntrar = findViewById<Button>(R.id.btn_entrar)
+
+        buttonEntrar.setOnClickListener{
+            val abrirDashboard = Intent (this,DashBoardActivity::class.java)
+            startActivity(abrirDashboard)
+        }
 
     }
 }
